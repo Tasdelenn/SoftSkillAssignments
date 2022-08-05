122ff57f15fe17196c2920cd75eeb3b3bcab1272
USER <hakann.tasdelenn@gmail.com>
2022-08-05 13:18:45 +0300

    First Task just ended

--- src/G2G12_Tasks/MufredatLink.java
+++ src/G2G12_Tasks/MufredatLink.java
@@ -3,15 +3,12 @@ package G2G12_Tasks;
 import Utils.GenelWebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.*;
-
-public class MufredatBtn extends GenelWebDriver {
+public class MufredatLink extends GenelWebDriver {
 
     @Test(priority = 1)
     void CredentialsLoginVal()
@@ -20,17 +17,19 @@ public class MufredatBtn extends GenelWebDriver {
         Assert.assertTrue(welCome.getText().toLowerCase().contains("welcome"));
     }
 
-    @Test()
+    @Test(priority = 2)
     void syllabusLink()
     {
         List<WebElement> syllabus = driver.findElements(By.xpath("//span[contains(text(),'Syllabus')]"));
 
-        System.out.println(syllabus.size());
+        System.out.println("Müfredat Linki Sayısı: " + syllabus.size());
+
 
         for (WebElement wE : syllabus)
         {
             Assert.assertNotNull(wE,"Karşılaştırma Sonucu");
             //Assert.assertTrue(wE.getText().toLowerCase().contains("syllabus"));
         }
+
     }
 }
